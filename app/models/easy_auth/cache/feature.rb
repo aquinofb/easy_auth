3c19c95ade633c846112ea6cc4ea3497d47c3666
aquinofb <aquinofb@gmail.com>
2016-02-28 10:06:12 -0300

    Refactoring

--- app/models/easy_auth/cache/feature.rb
+++ app/models/easy_auth/cache/feature.rb
@@ -2,13 +2,30 @@ module EasyAuth
   module Cache
     class Feature
       def self.key_cache authorizable_id
-        "#{Rails.application.class.parent_name}_easy_auth_allowed_features_#{authorizable_id}"
+        "easy_auth_allowed_features_#{authorizable_id}"
       end
 
-      def self.for_authorization authorizable_id, group_id
-        key = self.key_cache(authorizable_id)
-        self.append(key, self.hash_from_database(authorizable_id, group_id)) unless self.exists_for_group?(key, group_id)
-        Rails.cache.read(key)
+      def self.by_group authorizable_id, group_id
+        self.by_authorizable(authorizable_id)
+            .select { |f| f[:group_id].eql?(group_id) }
+      end
+
+      def self.by_authorizable authorizable_id
+        Rails.cache.read(self.key_cache(authorizable_id))
+      end
+
+      def self.update_list authorizable_id, group_id
+        self.append_new_features(authorizable_id, group_id) unless self.exists?(authorizable_id, group_id)
+      end
+
+      def self.exists? authorizable_id, group_id
+        Rails.cache.exist?(self.key_cache(authorizable_id))
+          && self.by_group(authorizable_id, group_id).any?
+      end
+
+      def self.append_new_features authorizable_id, group_id
+        hash = self.features_for(authorizable_id, group_id)
+        self.append(self.key_cache(authorizable_id), hash)
       end
 
       def self.append key, hash
@@ -16,13 +33,10 @@ module EasyAuth
         Rails.cache.write(key, entries.concat(hash))
       end
 
-      def self.exists_for_group? key, group_id
-        Rails.cache.exist?(key) && Rails.cache.read(key).detect {|e| e[:group_id].eql?(group_id)}.present?
-      end
-
-      def self.hash_from_database authorizable_id, group_id
-        features = Heimdall::Feature.by_group(group_id).by_authorizable(authorizable_id)
-        features.map {|feature| {group_id: group_id, namespace: feature.namespace, action: feature.action} }
+      def self.features_for authorizable_id, group_id
+        EasyAuth::Feature.by_authorizable(authorizable_id)
+                         .by_group(group_id)
+                         .map { |f| {group_id: group_id, namespace: f.namespace, action: f.action} }
       end
     end
   end
